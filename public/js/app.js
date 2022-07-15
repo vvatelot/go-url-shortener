83e733fa661a96fee7485e9073c7c73b1a13c420
Vincent Vatelot <vincent.vatelot@ik.me>
2022-07-15 16:54:01 +0200

    💄 ui(navbar): Update navbar to show on mobile

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
             $notification.parentNode.removeChild($notification);
         });
     });
+
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+        });
+    });
 });
 
 function switchLink(id) {
